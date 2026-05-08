b50cc701140e19cc5b30117570167b52b60caaeb
Vishwajeet Kumar <vishwajeetsingh52439@gmail.com>
2026-05-09 00:53:23 +0530

    Fix auth cookies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+app.set("trust proxy", 1);
 const mongoose = require("mongoose");
 const dns = require("dns");
 const cors = require("cors");
